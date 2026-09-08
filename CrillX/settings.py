186f5aefe1f9f6fcfa4620ff2b3ae8a85869b4dc
kishan <eng.kishan2024@gmail.com>
2026-09-09 01:31:29 +0530

    Finalize backend production CI/CD

--- CrillX/settings.py
+++ CrillX/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6o4%fl!x@)u!tjnm*1%&*5i0!v$)uhwlcaiwzom=3mt9v&f&-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '43.204.148.240']
 
 
 # Application definition
